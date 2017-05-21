59395512121412df57f4fce19313f4f6d8902d6d
cperez2187 <cperez2187@gmail.com>
2017-05-21 01:06:16 -0700

    update

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -26,7 +26,8 @@ $(document).ready(function() {
 
 	// On click event for every button
 	$(document).on('click', '.searchButton', function() {
-
+		// clear div after every new button press
+		$('#searches').empty();
 		// Hold data-type of each button in variable
 		let type = $(this).data('type');
 
@@ -39,8 +40,40 @@ $(document).ready(function() {
 			url: queryURL,
 			method: 'GET'
 		}).done(function(response) {
-			console.log(response);
+			
+			response.data.forEach(function(element) {
+				let searchDiv = $('<div class="search-item">');
+				let rating = element.rating;
+				let p = $('<p>').text('Rating: ' + rating);
+
+				// Hold 'still' and 'animated' url's of the gif
+				let animated = element.images.fixed_height.url;
+				let still = element.images.fixed_height_still.url;
+
+				// Create <img> div and set attributes
+				let image = $('<img>');
+				image.attr('src', still);
+				image.attr('data-still', still);
+				image.attr('data-animated', animated);
+				image.attr('data-state', 'still');
+				image.addClass('searchImage');
+				searchDiv.append(p);
+				// Append <img> onto 'searchDiv'...
+				searchDiv.append(image);
+				// ...then append 'searchDiv' onto div with id of 'searches'
+				$('#searches').append(searchDiv);
+			});
 		})
+	})
+
+	$('#addSearch').on('click', function() {
+		// Grab input value from 1st input tag
+		let newSearch = $('input').eq(0).val();
+		// Add newSearch into searchArray
+		searchArray.push(newSearch);
+
+		// Re-populate buttons on screen
+		populateButtons(searchArray, 'searchButton', '#buttonsArea');
 	})
 });
 
